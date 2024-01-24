352d61b4b854d2cc255cb0262b27d49b12ea9612
nurhossainfarid <faahsan1516@gmail.com>
2024-01-25 00:03:55 +0600

    update in auth

--- components/shared/Navbar.jsx
+++ components/shared/Navbar.jsx
@@ -1,84 +1,80 @@
-'use client'
+"use client";
 import Image from "next/image";
 
-import {
-    Sheet,
-    SheetContent,
-    SheetTrigger,
-} from "@/components/ui/sheet";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 
 import NavItems from "./NavItems";
 
-import {
-    DropdownMenu,
-    DropdownMenuContent,
-    DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
-
 import { Button } from "../ui/button";
 
-import {
-    Tabs,
-    TabsContent,
-    TabsList,
-    TabsTrigger,
-} from "@/components/ui/tabs"
-import LoginPage from "@/app/(auth)/login/page";
-
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import LoginRegistrationPage from "@/app/(auth)/login/page";
 
 const Navbar = () => {
+  return (
+    <nav className="flex justify-between  backdrop-blur-md items-center  md:px-10 sticky top-0 drop-shadow-md z-10 ">
+      <Image
+        src={"/assets/Kiti-Logo.png"}
+        width={200}
+        height={200}
+        alt="logo"
+      />
 
+      <div className="hidden  md:block text-lg font-normal">
+        <NavItems />
+      </div>
 
-    return (
-        <nav className="flex justify-between  backdrop-blur-md items-center  md:px-10 sticky top-0 drop-shadow-md z-10 ">
-            <Image src={'/assets/Kiti-Logo.png'} width={200} height={200} alt="logo" />
+      <div className="block md:hidden">
+        <Button
+          className="btn bg-primary "
+          onClick={() => document.getElementById("my_modal_4").showModal()}
+        >
+          Login/SignUp
+        </Button>
 
-            <div className="hidden  md:block text-lg font-normal">
-                <NavItems />
-            </div>
+        <dialog id="my_modal_4" className="modal ">
+          <div className="modal-box bg-white">
+            <form method="dialog">
+              {/* if there is a button in form, it will close the modal */}
+              <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-0">
+                ✕
+              </button>
+            </form>
+            <Tabs defaultValue="account" className="w-full ">
+              <TabsList className="grid w-full grid-cols-2 mt-2 bg-primary">
+                <TabsTrigger className="text-neutral-950" 
+                 value="login">Login</TabsTrigger>
+                <TabsTrigger className="text-neutral-950" 
+                value="registration">Registration</TabsTrigger>
+              </TabsList>
+              <TabsContent value="login">
+                <LoginRegistrationPage login={true} />
+              </TabsContent>
+              <TabsContent value="registration">
+                <LoginRegistrationPage />
+              </TabsContent>
+            </Tabs>
+          </div>
+        </dialog>
+      </div>
 
-          
+      <div className="md:hidden">
+        <Sheet>
+          <SheetTrigger>
+            <Image
+              src={"/assets/menu.png"}
+              width={40}
+              height={40}
+              alt="menubar_log"
+            />
+          </SheetTrigger>
+          <SheetContent>
+            <NavItems />
+          </SheetContent>
+        </Sheet>
+      </div>
+    </nav>
+  );
+};
 
-                <Button className="btn bg-primary " onClick={() => document.getElementById('my_modal_3').showModal()}>Login/SignUp</Button>
-
-                <dialog id="my_modal_3" className="bg-red-500  mx-auto rounded-md bg-white p-10 relative ">
-
-                    <form method="dialog" className=" absolute  right-2 top-2 " >
-                        {/* if there is a button in form, it will close the modal */}
-                        <button className="btn btn-sm btn-circle btn-ghost absolute  right-2 top-2">✕</button>
-                    </form>
-
-                    <Tabs defaultValue="login" >
-                        <TabsList className="grid w-full grid-cols-2 ">
-                            <TabsTrigger value="login">Login</TabsTrigger>
-                            <TabsTrigger value="signup">Signup</TabsTrigger>
-                        </TabsList>
-                        <TabsContent value="login">
-                          <h2>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias minima quidem numquam, beatae quibusdam, at magni animi provident iure enim quos architecto quisquam necessitatibus ullam voluptas optio iusto vero qui.</h2>
-                        </TabsContent>
-                        <TabsContent value="signup">
-                            <h2>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusantium ea eveniet tempore unde ex praesentium neque nobis eius aut ullam. Earum alias atque itaque dolores magnam quo. Ad, rem qui!</h2>
-                        </TabsContent>
-                    </Tabs>
-                </dialog>
-
-           
-
-            <div className="md:hidden" >
-                <Sheet>
-                    <SheetTrigger>
-                        <Image src={'/assets/menu.png'} width={40} height={40} alt="menubar_log" />
-                    </SheetTrigger>
-                    <SheetContent>
-                        <NavItems />
-                    </SheetContent>
-                </Sheet>
-            </div>
-
-
-        </nav>
-    )
-}
-
-export default Navbar
-
+export default Navbar;
