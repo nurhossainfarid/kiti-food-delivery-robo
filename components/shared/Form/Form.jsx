4651d86f28c041c900b36127ba77de171b425286
the-jahid <aenor007@gmail.com>
2024-01-24 22:49:26 -0600

    form_updated

--- components/shared/Form/Form.jsx
+++ components/shared/Form/Form.jsx
@@ -94,6 +94,7 @@ const Form = (props) => {
               {/* Email section end */}
               {/* password section */}
               <div>
+              <div>
                 <div className="flex items-center justify-between">
                   <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
                     Password
@@ -110,13 +111,14 @@ const Form = (props) => {
                     name="password"
                     type="password"
                     autoComplete="current-password"
-                    required
-                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                     {...register("password", { required: true })}
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                   />
+
                 </div>
+              </div>
                 {/* Registration Button */}
-                  <div>
+                <div>
                     <button
                       type="submit"
                       placeholder="Login"
@@ -124,7 +126,8 @@ const Form = (props) => {
                     >
                       Registration
                     </button>
-                  </div>
+                </div>
+                  
               </div>
             </>}
             {/* Login section */}
@@ -177,7 +180,7 @@ const Form = (props) => {
                 <button
                   type="submit"
                   placeholder="Login"
-                  className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                  className=" bg-primary block w-full rounded-md border-0 py-1.5 text-white shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 >
                   Login
                 </button>
