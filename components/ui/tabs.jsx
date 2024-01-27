d55a9fe9ef7bb8131a06a8e4d6c3b8e9a97b812b
nurhossainfarid <faahsan1516@gmail.com>
2024-01-27 23:01:58 +0600

    update food item section

--- components/ui/tabs.jsx
+++ components/ui/tabs.jsx
@@ -11,7 +11,7 @@ const TabsList = React.forwardRef(({ className, ...props }, ref) => (
   <TabsPrimitive.List
     ref={ref}
     className={cn(
-      "inline-flex h-10 items-center justify-center rounded-md bg-neutral-100 p-1 text-neutral-500 dark:bg-neutral-800 dark:text-neutral-400",
+      "inline-flex h-full items-center justify-center rounded-md bg-neutral-100 p-1 text-neutral-500 dark:bg-neutral-800 dark:text-neutral-400",
       className
     )}
     {...props} />
